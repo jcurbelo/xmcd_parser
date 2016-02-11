b1933a09a5ba08fc63deb165c354129400ad12ea
jcurbelo <curbelorobin@gmail.com>
2016-02-11 15:50:18 -0500

    Refactor code

--- xmcd_parser/utils/node_adaptor.py
+++ xmcd_parser/utils/node_adaptor.py
@@ -26,10 +26,20 @@ def _literal(dict, scope):
 
 
 def _identifier(dict, scope):
-    if 'ml:id' in dict:
-        return IdNode(raw_text=dict['ml:id']['#text'], xml_attr=dict['ml:id'], scope=scope)
-    if 'ml:min' in dict:
-        return
+    text = dict['ml:id']['#text']
+    default = lambda rt, xa, s: IdNode(raw_text=dict['ml:id']['#text'], xml_attr=dict['ml:id'], scope=scope)
+    return keywords.get(text, default)(dict['ml:id']['#text'], dict['ml:id'], scope)
+
+
+def _definition(dict, scope):
+    inner_dict = dict['ml:define']
+    left = {'ml:id': inner_dict['ml:id']}
+    body = {}
+    if 'ml:real' in inner_dict:
+        body = {'ml:real': inner_dict['ml:real']}
+    if 'ml:apply' in inner_dict:
+        body = {'ml:apply': inner_dict['ml:apply']}
+    return DefinitionNode(scope=scope, left=adaptor(left, scope), body=adaptor(body, scope))
 
 
 def adaptor(dict, scope):
@@ -37,13 +47,6 @@ def adaptor(dict, scope):
     if 'ml:real' in dict:
         return _literal(dict, scope)
     if 'ml:id' in dict:
-        return IdNode(raw_text=dict['ml:id']['#text'], xml_attr=dict['ml:id'], scope=scope)
+        return _identifier(dict, scope)
     if 'ml:define' in dict:
-        inner_dict = dict['ml:define']
-        left = {'ml:id': inner_dict['ml:id']}
-        body = {}
-        if 'ml:real' in inner_dict:
-            body = {'ml:real': inner_dict['ml:real']}
-        if 'ml:apply' in inner_dict:
-            body = {'ml:apply': inner_dict['ml:apply']}
-        return DefinitionNode(scope=scope, left=adaptor(left, scope), body=adaptor(body, scope))
+        return _definition(dict, scope)
